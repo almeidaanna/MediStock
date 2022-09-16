c9dfa9c7918c395850e9d5d50687921ad61f798c
anna-almeida <107173243+anna-almeida@users.noreply.github.com>
2022-09-16 19:08:40 +1000

    created contact, about , home and create account page.

---
+++ Assignment2/Controllers/RegistrationController.cs
@@ -0,0 +1,109 @@
+﻿using Assignment2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Assignment2.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Registration/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Registration/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Registration/Create
+        [HttpPost]
+        public ActionResult Create(FormOneViewModel model)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                String FirstName = model.FirstName;
+                String LastName = model.LastName;
+                String Email = model.Email;
+                String Phone = model.Phone;
+                String Password = model.Password;
+                String ConfirmPassword = model.ConfirmPassword;
+                String Role = model.Role;
+                String MedicalRegistration = model.MedicalRegistration;
+                String FacilityName = model.FacilityName;
+                String Location = model.Location;
+                ViewBag.FirstName = FirstName;
+                ViewBag.LastName = LastName;
+                ViewBag.Email = Email;
+                ViewBag.Phone = Phone;
+                ViewBag.Password = Password;
+                ViewBag.ConfirmPassword = ConfirmPassword;
+                ViewBag.Role = Role;
+                ViewBag.MedicalRegistration = MedicalRegistration;
+                ViewBag.FacilityName = FacilityName;
+                ViewBag.Location = Location;
+                return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Registration/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Registration/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Registration/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Registration/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Assignment2/Models/RegistrationViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Assignment2.Models
+{
+    public class RegistrationViewModel
+    {
+    }
+    public class FormOneViewModel
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email ID")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Phone Number")]
+        public string Phone { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+
+
+        [Required]
+        [Display(Name = "Role")]
+        public string Role { get; set; }
+
+        [Display(Name = "Medical Registration")]
+        public string MedicalRegistration { get; set; }
+        
+        [Display(Name = "Facility Name")]
+        public string FacilityName { get; set; }
+
+        [Display(Name = "Facility Address")]
+        public string Location { get; set; }
+    }
+}
