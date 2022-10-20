986ab332629b7575e9c7daddb5acb47a16ecb23e
anna-almeida <107173243+anna-almeida@users.noreply.github.com>
2022-10-20 22:08:12 +1100

    Email working

---
+++ Assignment2/Controllers/EquipmentBookingsController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Assignment2.Models;
+using Assignment2.Utils;
+
+namespace Assignment2.Controllers
+{
+    public class EquipmentBookingsController : Controller
+    {
+        private FIT5032_MediStockContainer db = new FIT5032_MediStockContainer();
+
+        // GET: EquipmentBookings
+        public ActionResult Index()
+        {
+            var equipmentBookings = db.EquipmentBookings.Include(e => e.Doctor).Include(e => e.Logistic).Include(e => e.Equipment);
+            return View(equipmentBookings.ToList());
+        }
+
+        // GET: EquipmentBookings/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EquipmentBooking equipmentBooking = db.EquipmentBookings.Find(id);
+            if (equipmentBooking == null)
+            {
+                return HttpNotFound();
+            }
+            return View(equipmentBooking);
+        }
+
+        // GET: EquipmentBookings/Create
+        public ActionResult Create()
+        {
+            ViewBag.DoctorId = new SelectList(db.Doctors, "Id", "first_name");
+            ViewBag.LogisticId = new SelectList(db.Logistics, "Id", "first_name");
+            ViewBag.EquipmentId = new SelectList(db.Equipments, "Id", "equipment_name");
+            return View();
+        }
+
+        // POST: EquipmentBookings/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,datetime,status,quantity,DoctorId,LogisticId,EquipmentId")] EquipmentBooking equipmentBooking)
+        {
+            if (ModelState.IsValid)
+            {
+                db.EquipmentBookings.Add(equipmentBooking);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.DoctorId = new SelectList(db.Doctors, "Id", "first_name", equipmentBooking.DoctorId);
+            ViewBag.LogisticId = new SelectList(db.Logistics, "Id", "first_name", equipmentBooking.LogisticId);
+            ViewBag.EquipmentId = new SelectList(db.Equipments, "Id", "equipment_name", equipmentBooking.EquipmentId);
+            return View(equipmentBooking);
+        }
+
+        // GET: EquipmentBookings/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EquipmentBooking equipmentBooking = db.EquipmentBookings.Find(id);
+            if (equipmentBooking == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DoctorId = new SelectList(db.Doctors, "Id", "first_name", equipmentBooking.DoctorId);
+            ViewBag.LogisticId = new SelectList(db.Logistics, "Id", "first_name", equipmentBooking.LogisticId);
+            ViewBag.EquipmentId = new SelectList(db.Equipments, "Id", "equipment_name", equipmentBooking.EquipmentId);
+            return View(equipmentBooking);
+        }
+
+        // POST: EquipmentBookings/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,datetime,status,quantity,DoctorId,LogisticId,EquipmentId")] EquipmentBooking equipmentBooking)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(equipmentBooking).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DoctorId = new SelectList(db.Doctors, "Id", "first_name", equipmentBooking.DoctorId);
+            ViewBag.LogisticId = new SelectList(db.Logistics, "Id", "first_name", equipmentBooking.LogisticId);
+            ViewBag.EquipmentId = new SelectList(db.Equipments, "Id", "equipment_name", equipmentBooking.EquipmentId);
+            return View(equipmentBooking);
+        }
+
+        // GET: EquipmentBookings/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EquipmentBooking equipmentBooking = db.EquipmentBookings.Find(id);
+            if (equipmentBooking == null)
+            {
+                return HttpNotFound();
+            }
+            return View(equipmentBooking);
+        }
+
+        // POST: EquipmentBookings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            EquipmentBooking equipmentBooking = db.EquipmentBookings.Find(id);
+            db.EquipmentBookings.Remove(equipmentBooking);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        [HttpPost]
+        public ActionResult Details(SendEmailViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    EmailSender es = new EmailSender();
+                    es.Send();
+
+                    ViewBag.Result = "A mail has been sent to the logistics team and your registerd email-Id";
+
+                    ModelState.Clear();
+
+                    return View();
+                }
+                catch
+                {
+                    return View();
+                }
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Assignment2/Controllers/LogisticsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Assignment2.Models;
+
+namespace Assignment2.Controllers
+{
+    public class LogisticsController : Controller
+    {
+        private FIT5032_MediStockContainer db = new FIT5032_MediStockContainer();
+
+        // GET: Logistics
+        public ActionResult Index()
+        {
+            return View(db.Logistics.ToList());
+        }
+
+        // GET: Logistics/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Logistic logistic = db.Logistics.Find(id);
+            if (logistic == null)
+            {
+                return HttpNotFound();
+            }
+            return View(logistic);
+        }
+
+        // GET: Logistics/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Logistics/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,first_name,last_name,phone_no,email_id,password")] Logistic logistic)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Logistics.Add(logistic);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(logistic);
+        }
+
+        // GET: Logistics/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Logistic logistic = db.Logistics.Find(id);
+            if (logistic == null)
+            {
+                return HttpNotFound();
+            }
+            return View(logistic);
+        }
+
+        // POST: Logistics/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,first_name,last_name,phone_no,email_id,password")] Logistic logistic)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(logistic).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(logistic);
+        }
+
+        // GET: Logistics/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Logistic logistic = db.Logistics.Find(id);
+            if (logistic == null)
+            {
+                return HttpNotFound();
+            }
+            return View(logistic);
+        }
+
+        // POST: Logistics/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Logistic logistic = db.Logistics.Find(id);
+            db.Logistics.Remove(logistic);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
